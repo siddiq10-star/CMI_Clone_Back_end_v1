5d5d93bd7b66a4ed1c17b7cb2d7a9618c899d645
Mohammed Sidiq <4mh19cs053@gmail.com>
2026-02-12 11:19:01 +0530

    fixed categories

--- index.js
+++ index.js
@@ -10,20 +10,25 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:5173"];
-
 app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `❌ The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: '*',
   credentials: true,
 }));
+
+// const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:5173"];
+
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     // allow requests with no origin (like Postman)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = `❌ The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true,
+// }));
 
 
 
